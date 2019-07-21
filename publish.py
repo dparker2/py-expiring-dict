0f2a6c5dd37b83bd7aa3f44d9d61f2d0817062e0
David Parker <crazdave@gmail.com>
2019-07-21 13:47:59 -0700

    publish 0.1.0

--- publish.py
+++ publish.py
@@ -1,8 +1,12 @@
 import os
 import sys
+import shutil
 
 # Ensure dependencies
 os.system(f"{sys.executable} -m pip install --user --upgrade setuptools wheel twine")
+
+# Delete previous build
+shutil.rmtree("dist")
 
 # Build
 os.system(f"{sys.executable} setup.py sdist bdist_wheel")
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="expiring-dict",
-    version="0.0.1",
+    version="0.1.0",
     author="David Parker",
     description="Python dict with TTL support for auto-expiring caches",
     long_description=long_description,
