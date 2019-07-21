a7932664c5ec8c45a877e9d44e68fdf10ad3fb2a
David Parker <crazdave@gmail.com>
2019-07-21 13:31:52 -0700

    make it

--- expiring_dict/expiringdict.py
+++ expiring_dict/expiringdict.py
@@ -1,18 +1,107 @@
 from collections.abc import MutableMapping
+from threading import Timer
 
 
 class ExpiringDict(MutableMapping):
-    def __delitem__(self):
-        pass
+    def __init__(self, ttl=None):
+        """
+        Create an ExpiringDict class, optionally passing in a time-to-live
+        number in seconds that will act globally as an expiration time for keys.
 
-    def __getitem__(self):
-        pass
-    
+        If omitted, the dict will work like a normal dict by default, expiring
+        only keys explicity set via the `.ttl` method.
+        """
+        self.__store = dict()
+        self.__expirations = dict()
+        self.__ttl = ttl
+
+    def __delitem__(self, key):
+        """
+        Delete `key` from the dict.
+        Raises `KeyError` if key does not exist.
+        """
+        try:
+            del self.__store[key]
+        except KeyError:
+            raise KeyError
+        try:
+            self.__expirations[key].cancel()
+            del self.__expirations[key]
+        except KeyError:
+            pass
+
+    def __getitem__(self, key):
+        """
+        Return value of `key` in dict.
+        Raises `KeyError` if key does not exist.
+        """
+        try:
+            return self.__store[key]
+        except KeyError:
+            raise KeyError
+
+    def get(self, key, default=None):
+        """
+        Return value of `key` in dict, or default value which defaults to `None`.
+        """
+        return self.__store.get(key, default)
+
     def __iter__(self):
-        pass
-    
+        """
+        Return iterator of dict.
+        """
+        return iter(self.__store)
+
     def __len__(self):
-        pass
-    
-    def __setitem__(self):
-        pass
+        """
+        Return length of dict.
+        """
+        return len(self.__store)
+
+    def keys(self):
+        """
+        Return dict keys.
+        """
+        return self.__store.keys()
+
+    def values(self):
+        """
+        Return dict values.
+        """
+        return self.__store.values()
+
+    def items(self):
+        """
+        Return dict items.
+        """
+        return self.__store.items()
+
+    def __setitem__(self, key, value):
+        """
+        Set `value` of `key` in dict. `key` will be automatically
+        deleted if the `ttl` option was provided for this object.
+        """
+        if self.__ttl:
+            self.__set_with_expire(key, value, self.__ttl)
+        else:
+            self.__store[key] = value
+
+    def ttl(self, key, value, ttl):
+        """
+        Set `value` of `key` in dict to expire after `ttl` seconds.
+        Overrides object level `ttl` setting.
+        """
+        self.__set_with_expire(key, value, ttl)
+
+    def __set_with_expire(self, key, value, ttl):
+        def expire():
+            try:
+                del self[key]
+            except KeyError:
+                pass
+
+        timer = Timer(ttl, expire)
+        timer.start()
+        self.__expirations[key] = timer
+
+        self.__store[key] = value
--- tests/test_expiringdict.py
+++ tests/test_expiringdict.py
@@ -1,4 +1,44 @@
 from expiring_dict import ExpiringDict
+from time import sleep
+
 
 def test_init():
     ExpiringDict()
+
+
+def test_no_ttl():
+    d = ExpiringDict()
+    d["key"] = "value"
+    assert len(d._ExpiringDict__expirations) == 0
+
+
+def test_class_ttl():
+    d = ExpiringDict(ttl=1)
+    d["key"] = "should be gone"
+    assert len(d) == 1
+    sleep(1.1)
+    assert len(d) == 0
+
+
+def test_set_ttl():
+    d = ExpiringDict()
+    d.ttl("key", "expire", 1)
+    assert len(d) == 1
+    sleep(1.1)
+    assert len(d) == 0
+
+
+def test_dict_ops():
+    ed = ExpiringDict()
+    ed["one"] = 1
+    ed["two"] = 2
+    ed["three"] = 3
+    d = dict()
+    d["one"] = 1
+    d["two"] = 2
+    d["three"] = 3
+    assert [x for x in d] == [x for x in ed]
+    assert [k for k in d.keys()] == [k for k in ed.keys()]
+    assert [v for v in d.values()] == [v for v in ed.values()]
+    assert [i for i in d.items()] == [i for i in ed.items()]
+
