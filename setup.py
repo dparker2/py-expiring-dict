f08b0bf61726f9876d304af4be04161b21cb6fda
WarrierRajeev <warrier.rajeev.95@gmail.com>
2020-03-29 17:57:18 +0530

    Update version to 1.0.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="expiring-dict",
-    version="1.0.0",
+    version="1.0.1",
     author="David Parker",
     description="Python dict with TTL support for auto-expiring caches",
     long_description=long_description,
